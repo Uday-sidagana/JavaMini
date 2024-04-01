2bb868e6ade4503d09c38b0916b55b62078398a3
Uday-Sidagana <uday.sidgana@gmail.com>
2024-04-01 23:30:37 +0530

    feat: add User.java code

--- src/User.java
+++ src/User.java
@@ -1,2 +1,13 @@
 public class User {
+    private String name;
+    private String userID;
+
+    public User(String name, String userID) {
+        this.name = name;
+        this.userID = userID;
+    }
+
+    // Getters
+    public String getName() { return name; }
+    public String getUserID() { return userID; }
 }
