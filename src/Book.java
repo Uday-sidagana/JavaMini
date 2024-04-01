b69f494b918c431369aebca2d50e9ea1b3e1ac19
Uday-Sidagana <uday.sidgana@gmail.com>
2024-04-01 23:28:38 +0530

    feat: add code to Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,20 @@
 public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private boolean isAvailable;
+
+    public Book(String title, String author, String ISBN) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.isAvailable = true; // By default, a new book is available
+    }
+
+    // Getters and Setters
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getISBN() { return ISBN; }
+    public boolean isAvailable() { return isAvailable; }
+    public void setAvailable(boolean available) { isAvailable = available; }
 }
